70963ac21967f033f392bcce4aa8af3e13d5e614
Eamon McCarron <mccarronea@gmail.com>
2023-10-22 08:19:53 -0300

    refactor: Abstract compendium parsing

--- src/app/api/compendium/ancestries/route.ts
+++ src/app/api/compendium/ancestries/route.ts
@@ -1,11 +1,18 @@
 import { DBConnection } from '@/app/lib/db/DBConnection';
+import { formatCompendiumJSON } from '@/app/lib/compendium/formatCompendiumJSON';
+import type { NextRequest } from 'next/server';
 
 export const revalidate = 3;
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   const db = await DBConnection.getCompendiumDB();
 
-  const ancestries = await db.collection('ancestries').find().toArray();
+  const ancestries = formatCompendiumJSON(
+    await db.collection('ancestries').find().toArray()
+  );
+
+  // const searchParams = req.nextUrl.searchParams;
+  // const query = searchParams.get('suggested_for_character');
 
   return Response.json(
     ancestries.map((ancestry) => {
