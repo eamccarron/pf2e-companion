b27cd104c14ba456107d29d9d980de8ccc4865f5
Eamon McCarron <mccarronea@gmail.com>
2023-10-27 06:27:15 -0300

    refactor: remove api directory

--- apps/dashboard/app/routes/api/compendium/feats/route.ts
+++
@@ -1,8 +0,0 @@
-import { DBConnection } from '@pf2-companion/db';
-
-export async function GET() {
-  const db = await DBConnection.getCompendiumDB();
-
-  const feats = await db.collection('feats').find().toArray();
-  return Response.json(feats);
-}
