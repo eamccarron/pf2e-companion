c89be9ca26aafe3f8e7057973e2701456c98afe3
Eamon McCarron <mccarronea@gmail.com>
2023-11-22 08:45:24 -0400

    ci: Cache npm installation, next.js prod build fix

--- apps/dashboard-remix/app/routes/character-builder.heritages.$id.ts
+++
@@ -1,5 +0,0 @@
-import { LoaderFunctionArgs } from '@remix-run/node';
-import { heritageLoader } from '@pf2-companion/character-builder/feature/server';
-
-export const loader = async ({ params }: LoaderFunctionArgs) =>
-  params?.id ? heritageLoader(params.id) : [];
